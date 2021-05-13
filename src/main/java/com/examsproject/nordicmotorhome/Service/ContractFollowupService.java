73dd4b59dc22321ff0f9d1822a8caf90db1e1e74
Raskoe99 <r.koefoed@hotmail.com>
2021-05-13 15:51:19 +0200

    Fuck der er mange serviceklasser

--- src/main/java/com/examsproject/nordicmotorhome/Service/ContractFollowupService.java
+++ src/main/java/com/examsproject/nordicmotorhome/Service/ContractFollowupService.java
@@ -1,4 +1,32 @@
 package com.examsproject.nordicmotorhome.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * service class til contract
+ *@author rasmuskoefoed
+ */
+
+@Service
+
 public class ContractFollowupService {
+
+    @Autowired
+    ContractFollowup contractFollowup;
+    public List<ContractFollowup> fetchAll(){
+        return ContractFollowupRepo.fetchAll();
+    }
+    public ContractFollowup addContractFollowup(ContractFollowup c){
+        return contractFollowupRepo.addContractFollowup(c);
+    }
+    public ContractFollowup findContractFollowupById(int contractFollowupID){
+        return contractFollowupRepo.findContractFollowupByID(contractFollowupID);
+    }
+    public Boolean deleteContractFollowup(int contractFollowupID){
+        return contractFollowupRepo.deleteContractFollowup(contractFollowupID);
+    }
+    public contractFollowup updateContractFollowup(int contractFollowupID, ContractFollowup c){
+        return contractFollowupRepo.updateContractFollowup(contractFollowupID,c);
+    }
 }
--- src/main/java/com/examsproject/nordicmotorhome/Service/CustomerService.java
+++ src/main/java/com/examsproject/nordicmotorhome/Service/CustomerService.java
@@ -1,6 +1,5 @@
 package com.examsproject.nordicmotorhome.Service;
 
-import com.examsproject.nordicmotorhome.Model.Contract;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
