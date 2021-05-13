95f7ffd1fad8dd243be52001df3cc1dead413ee6
jona881a <jonaskunert@hotmail.com>
2021-05-13 12:35:18 +0200

    Created project

---
+++ src/main/java/com/examsproject/nordicmotorhome/NordicmotorhomeApplication.java
@@ -0,0 +1,13 @@
+package com.examsproject.nordicmotorhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordicmotorhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordicmotorhomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examsproject/nordicmotorhome/NordicmotorhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examsproject.nordicmotorhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordicmotorhomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
