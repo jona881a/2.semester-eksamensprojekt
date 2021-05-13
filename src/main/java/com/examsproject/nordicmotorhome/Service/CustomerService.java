3586db8c4ac71fa411baae95cb624c15bb13e385
Raskoe99 <r.koefoed@hotmail.com>
2021-05-13 15:37:51 +0200

    .

--- src/main/java/com/examsproject/nordicmotorhome/Service/CustomerService.java
+++ src/main/java/com/examsproject/nordicmotorhome/Service/CustomerService.java
@@ -1,4 +1,33 @@
 package com.examsproject.nordicmotorhome.Service;
 
+import com.examsproject.nordicmotorhome.Model.Contract;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * service class til customer
+ *@author rasmuskoefoed
+ */
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    CustomerRepo customerRepo;
+    public List<Customer> fetchAll(){
+        return customerRepo.fetchAll();
+    }
+    public Customer addCustomer(Customer c){
+        return customerRepo.addCustomer(c);
+    }
+    public Customer findCustomerById(int customerID){
+        return customerRepo.findCustomerByID(customerID);
+    }
+    public Boolean deleteCustomer(int customerID){
+        return customerRepo.deleteCustomer(customerID);
+    }
+    public Customer updateCustomer(int customerID, Customer c){
+        return customerRepo.updateCustomer(customerID,c);
+    }
 }
