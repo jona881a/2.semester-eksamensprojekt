66eb907b6a56039165f4a1556526b0ca8dabcccf
Raskoe99 <r.koefoed@hotmail.com>
2021-05-13 15:51:46 +0200

    .

--- src/main/java/com/examsproject/nordicmotorhome/Service/ContractFollowupService.java
+++ src/main/java/com/examsproject/nordicmotorhome/Service/ContractFollowupService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * service class til contract
+ * service class til contractFollowup
  *@author rasmuskoefoed
  */
 
