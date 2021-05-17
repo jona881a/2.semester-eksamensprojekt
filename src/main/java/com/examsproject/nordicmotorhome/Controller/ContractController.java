f5dde98da4240e8a60f4086d1cdc98735fba14fe
jona881a <jonaskunert@hotmail.com>
2021-05-17 13:45:30 +0200

    CSS IS WORKING

--- src/main/java/com/examsproject/nordicmotorhome/Controller/ContractController.java
+++ src/main/java/com/examsproject/nordicmotorhome/Controller/ContractController.java
@@ -1,18 +1,25 @@
 package com.examsproject.nordicmotorhome.Controller;
 
+import com.examsproject.nordicmotorhome.Model.Autocamper;
 import com.examsproject.nordicmotorhome.Model.Contract;
+import com.examsproject.nordicmotorhome.Service.AutocamperService;
 import com.examsproject.nordicmotorhome.Service.ContractService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ContractController {
 
     @Autowired
     ContractService contractService;
+    @Autowired
+    AutocamperService autocamperService;
 
     @GetMapping("/contract/contractIndex")
     public String contractIndex() {
@@ -20,13 +27,14 @@ public class ContractController {
     }
 
     @GetMapping("/contract/contractCreate")
-    public String contractCreate() {
+    public String contractCreate(Model model) {
+        List<Autocamper> autocampers = autocamperService.fetchAll();
+        model.addAttribute("autocampers",autocampers);
         return "home/contract/contractCreate";
     }
 
-    @PostMapping("/contract/contractIndex")
+    @PostMapping("/contract/contractCreate")
     public String contractCreate(@ModelAttribute Contract c) {
-        contractService.calculateRentalPrice(c);
         return "home/contract/contractCreate";
     }
 }
