91bf4f71bd07ac8f8c187a464d2a63272f9eb29c
jona881a <jonaskunert@hotmail.com>
2021-05-17 12:47:25 +0200

    Made a bunch

--- src/main/java/com/examsproject/nordicmotorhome/Controller/ContractController.java
+++ src/main/java/com/examsproject/nordicmotorhome/Controller/ContractController.java
@@ -1,10 +1,18 @@
 package com.examsproject.nordicmotorhome.Controller;
 
+import com.examsproject.nordicmotorhome.Model.Contract;
+import com.examsproject.nordicmotorhome.Service.ContractService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ContractController {
+
+    @Autowired
+    ContractService contractService;
 
     @GetMapping("/contract/contractIndex")
     public String contractIndex() {
@@ -13,6 +21,12 @@ public class ContractController {
 
     @GetMapping("/contract/contractCreate")
     public String contractCreate() {
+        return "home/contract/contractCreate";
+    }
+
+    @PostMapping("/contract/contractIndex")
+    public String contractCreate(@ModelAttribute Contract c) {
+        contractService.calculateRentalPrice(c);
         return "home/contract/contractCreate";
     }
 }
