296dcf55ea7f6c731eb03ed644e822503404f3e2
Raskoe99 <r.koefoed@hotmail.com>
2021-05-18 09:31:45 +0200

    .

--- src/main/java/com/examsproject/nordicmotorhome/Controller/HomeController.java
+++ src/main/java/com/examsproject/nordicmotorhome/Controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String index() {
         return "home/index";
     }
+
+    @GetMapping("/loginIndex")
+    public String loginIndex() {
+        return "home/loginIndex";
+    }
 }
