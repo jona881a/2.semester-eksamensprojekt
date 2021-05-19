1e2125818baba5cad8f199985ff828f514e36383
Raskoe99 <r.koefoed@hotmail.com>
2021-05-19 16:09:58 +0200

    .

--- src/main/java/com/examsproject/nordicmotorhome/Controller/ContractController.java
+++ src/main/java/com/examsproject/nordicmotorhome/Controller/ContractController.java
@@ -2,6 +2,7 @@ package com.examsproject.nordicmotorhome.Controller;
 
 import com.examsproject.nordicmotorhome.Model.Autocamper;
 import com.examsproject.nordicmotorhome.Model.Contract;
+import com.examsproject.nordicmotorhome.Model.Extras;
 import com.examsproject.nordicmotorhome.Service.AutocamperService;
 import com.examsproject.nordicmotorhome.Service.ContractService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.Period;
 import java.util.List;
 
 /**
@@ -23,8 +21,6 @@ import java.util.List;
 @Controller
 public class ContractController {
 
-    private static final double MIDDLE_SEASON = 0.3;
-    private static final double PEAK_SEASON= 0.6;
 
     @Autowired
     ContractService contractService;
@@ -63,31 +59,7 @@ public class ContractController {
      */
     @PostMapping("/contract/contractCreate")
     public String contractCreate(@ModelAttribute Contract c, @ModelAttribute Extras e) {
-        double rentalPrice = 0.0;
-        LocalDate localDateStart = LocalDate.parse(c.getRentalStartDate());
-        LocalDate localDateEnd = LocalDate.parse(c.getRentalStartDate());
-
-        //Udregn samlede dage bilen er lejet og gang det med bilens daglige pris
-        //Gang desuden beløbet med attributter hvis det er højsæson/middelsæson
-        LocalDate date1 = LocalDate.of(localDateStart.getYear(), localDateStart.getMonth(), localDateStart.getDayOfMonth());
-        LocalDate date2 = date1.with(Month.from(localDateEnd.getMonth())).withDayOfMonth(localDateEnd.getDayOfMonth());
-        int numDays = Period.between(date1, date2).getDays();
-        if (numDays == 0) {
-            numDays = 1;
-        }
-
-        rentalPrice += + numDays * autocamperService.findAutocamperByID(c.getAutocamperID()).getPrice();
-
-        if (localDateEnd.getMonthValue() == 5) {
-
-        }
-
-        /* @TODO vi skal have lavet det om så den kun er "ikke tilgængelig" i den periode den er lejet i
-        Autocamper a = autocamperService.findAutocamperByID(c.getAutocamperID());
-        a.setIsAvailable("no");
-        autocamperService.updateAutocamper(c.getAutocamperID(),a);
-
-         */
+        contractService.calculateTotalContractPrice(c,e,autocamperService);
         contractService.createContract(c,e);
         return "redirect:/";
     }
--- src/main/java/com/examsproject/nordicmotorhome/Service/ContractService.java
+++ src/main/java/com/examsproject/nordicmotorhome/Service/ContractService.java
@@ -7,6 +7,9 @@ import com.examsproject.nordicmotorhome.Repository.ContractRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 import java.util.List;
 
 /**
@@ -16,6 +19,9 @@ import java.util.List;
 
 @Service
 public class ContractService {
+
+    private static final double MIDDLE_SEASON = 0.3;
+    private static final double PEAK_SEASON= 0.6;
 
     @Autowired
     ContractRepo contractRepo;
@@ -34,5 +40,51 @@ public class ContractService {
     }
     public Contract updateContract(int contractID, Contract c){
         return contractRepo.updateContract(contractID,c);
+    }
+
+    public void calculateTotalContractPrice(Contract c, Extras e, AutocamperService autocamperService) {
+        double rentalPrice = 0.0;
+        LocalDate localDateStart = LocalDate.parse(c.getRentalStartDate());
+        LocalDate localDateEnd = LocalDate.parse(c.getRentalStartDate());
+
+        //Udregn samlede dage bilen er lejet og gang det med bilens daglige pris
+        //Gang desuden beløbet med attributter hvis det er højsæson/middelsæson
+        LocalDate date1 = LocalDate.of(localDateStart.getYear(), localDateStart.getMonth(), localDateStart.getDayOfMonth());
+        LocalDate date2 = date1.with(Month.from(localDateEnd.getMonth())).withDayOfMonth(localDateEnd.getDayOfMonth());
+        int numDays = Period.between(date1, date2).getDays();
+        if (numDays == 0) {
+            numDays = 1;
+        }
+
+        rentalPrice += numDays * autocamperService.findAutocamperByID(c.getAutocamperID()).getPriceperday();
+
+        if (e.getFamilyPackage().equals("yes")) {
+            rentalPrice += 750;
+        } else if (e.getSportPackage().equals("yes")) {
+            rentalPrice += 500;
+        } else if (e.getLuxuryPackage().equals("yes")) {
+            rentalPrice += 1000;
+        } else if (e.getPicknickPackage().equals("yes")) {
+            rentalPrice += 250;
+        }
+
+        if (localDateEnd.getMonthValue() == 5) {
+            rentalPrice *= PEAK_SEASON;
+        } else if (localDateEnd.getMonthValue() == 6) {
+            rentalPrice *= PEAK_SEASON;
+        } else if (localDateEnd.getMonthValue() == 7) {
+            rentalPrice *= PEAK_SEASON;
+        } else if (localDateEnd.getMonthValue() == 8) {
+            rentalPrice *= PEAK_SEASON;
+        } else if (localDateEnd.getMonthValue() == 3) {
+            rentalPrice *= MIDDLE_SEASON;
+        } else if (localDateEnd.getMonthValue() == 4) {
+            rentalPrice *= MIDDLE_SEASON;
+        } else if (localDateEnd.getMonthValue() == 9) {
+            rentalPrice *= MIDDLE_SEASON;
+        } else if (localDateEnd.getMonthValue() == 10) {
+            rentalPrice *= MIDDLE_SEASON;
+        }
+        c.setRentalPrice(rentalPrice);
     }
 }
